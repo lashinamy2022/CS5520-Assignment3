ef2640c13938957d502a5c4b441d187fe4bf767c
yusun97 <yusun.apply@outlook.com>
2023-03-26 17:54:06 -0700

    edit the squre component

--- components/Square.js
+++ components/Square.js
@@ -1,10 +1,19 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet, Dimensions } from "react-native";
+import React from "react";
 
 export default function Square() {
   return (
-    <View>
+    <View style={styles.SquareItem}>
       <Text>Square</Text>
     </View>
-  )
+  );
 }
+
+const styles = StyleSheet.create({
+  SquareItem: {
+    height: 300,
+    width: Dimensions.get("window") / 2 - 16,
+    borderRadius: 5,
+    backgroundColor: "red",
+  },
+});
