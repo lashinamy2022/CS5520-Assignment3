cd8dd68c54511e985019bbb9d92655e1307107fb
yusun97 <yusun.apply@outlook.com>
2023-03-27 16:28:08 -0700

    add settings to MeScreen

--- screens/MeScreen.js
+++ screens/MeScreen.js
@@ -1,17 +1,21 @@
 import { View, Text, StyleSheet, Dimensions, Image } from "react-native";
 import React from "react";
+import PressableArea from "../components/PressableArea";
 
 export default function MeScreen() {
   return (
-    <View>
+    <View style={styles.container}>
       <View style={styles.topContainer}>
         <View style={styles.rowContainer}>
           <Image
             style={styles.image}
             source={require("../assets/scenery.jpg")}
           />
-          <Text>MeScreen</Text>
+          <Text style={styles.IDText}>MeScreen</Text>
         </View>
+        <PressableArea>
+          <Text>Setting</Text>
+        </PressableArea>
       </View>
     </View>
   );
@@ -26,6 +30,7 @@ const styles = StyleSheet.create({
   },
   rowContainer: {
     flexDirection: "row",
+    alignItems: "center",
   },
   image: {
     height: 80,
@@ -34,5 +39,10 @@ const styles = StyleSheet.create({
     borderWidth: 2,
     borderColor: "white",
     marginLeft: 15,
+  },
+  IDText: {
+    fontSize: 20,
+    margin: 10,
+    color: "white",
   },
 });
