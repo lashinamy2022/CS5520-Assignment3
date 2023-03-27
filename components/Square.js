e1ca3f799a3ea06bb35ad08f199b67d78571f68a
yusun97 <yusun.apply@outlook.com>
2023-03-27 16:00:06 -0700

    small changes

--- components/Square.js
+++ components/Square.js
@@ -10,7 +10,7 @@ export default function Square({ image, title, id, userPhoto }) {
   }
 
   function showDetails() {
-    console.log("show details");
+    console.log("show details"); 
   }
 
   return (
