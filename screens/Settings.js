468395c35ac5f19d72af97f5c513de5d2e7772cc
Max <erin@Maxs-MacBook-Pro.local>
2023-03-29 14:52:51 -0700

    add edit username function

--- screens/Settings.js
+++ screens/Settings.js
@@ -1,33 +1,98 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, TextInput } from "react-native";
 import { useState } from "react";
 import React from "react";
 import PressableArea from "../components/PressableArea";
 
 export default function Setting() {
-  const [show, setShow] = useState(false);
-  const handlePress = () => {
-    setShow(true);
-  };
+  //   const [username, setUsername] = useState("Max");
+  //   const [showName, setShowname] = useState(false);
+  //   const [showPassword, setShowpassword] = useState(false);
+
+  //   const handleNamePress = () => {
+  //     setShowname(true);
+  //   };
+  //   const handlePasswordPress = () => {
+  //     setShowpassword(true);
+  //   };
+  //   return (
+  //     <View style={styles.container}>
+  //       <View style={styles.nameContainer}>
+  //         <PressableArea areaPressed={handleNamePress}>
+  //           <Text style={styles.text}>Username: Me</Text>
+  //         </PressableArea>
+  //         {showName && (
+  //           <TextInput
+  //             style={styles.input}
+  //             value={username}
+  //             onChangeText={setUsername}
+  //           />
+  //         )}
+  //       </View>
+  //       <View style={styles.passwordContainer}>
+  //         <PressableArea areaPressed={handlePasswordPress}>
+  //           <Text style={styles.text}>Show me</Text>
+  //         </PressableArea>
+  //         {showPassword && <Text>Hi</Text>}
+  //       </View>
+  //     </View>
+  //   );
+  const [username, setUsername] = useState("Me");
+  const [showName, setShowName] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleNamePress = () => setShowName(!showName);
+  const handlePasswordPress = () => setShowPassword(!showPassword);
+
   return (
     <View style={styles.container}>
-      <Text style={styles.name}>Name:</Text>
-      <PressableArea areaPressed={handlePress} customizedStyle={styles.pressed}>
-        <Text style={styles.pressed}>Show me</Text>
-      </PressableArea>
-      {show && <Text>Hi</Text>}
+      <View style={styles.nameContainer}>
+        <View style={styles.usernameContainer}>
+          <Text style={styles.text}>Username: </Text>
+          {showName ? (
+            <TextInput
+              style={styles.text}
+              value={username}
+              onChangeText={setUsername}
+            />
+          ) : (
+            <PressableArea areaPressed={handleNamePress}>
+              <Text style={styles.text}> {username}</Text>
+            </PressableArea>
+          )}
+        </View>
+      </View>
+      {/* <View style={styles.passwordContainer}>
+        <PressableArea areaPressed={handlePasswordPress}>
+          <Text style={styles.text}>Show me</Text>
+        </PressableArea>
+        {showPassword && <Text>Hi</Text>}
+      </View> */}
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: { flex: 1 },
-  name: {},
-
-  pressed: {
-    // flex: 1,
-    // marginTop: "20%",
+  nameContainer: {
+    flex: 1,
     justifyContent: "center",
     alignItems: "center",
+  },
+
+  passwordContainer: {
+    flex: 2,
+    // // flex: 1,
+    // // marginTop: "20%",
+    // justifyContent: "center",
+    alignItems: "center",
+    // fontSize: 20,
+  },
+
+  text: {
     fontSize: 20,
+  },
+
+  usernameContainer: {
+    flexDirection: "row",
   },
 });
