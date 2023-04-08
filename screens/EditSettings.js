5c10f2b1d74d54800154df03ce3f4975e16ee14f
yusun97 <yusun.apply@outlook.com>
2023-04-07 22:26:09 -0700

    add reset button and use updatePassword

--- screens/EditSettings.js
+++ screens/EditSettings.js
@@ -1,10 +1,161 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet, Image, TextInput } from "react-native";
+import React, { useState } from "react";
+import { auth } from "../firebase/firebase-setup";
+import Label from "../components/Label";
+import PressableArea from "../components/PressableArea";
+import ErrorText from "../components/ErrorText";
+import { updatePassword } from "firebase/auth";
+import { updateUserInfo } from "../firebase/firebase-helper";
 
-export default function EditSettings() {
+export default function EditSettings({ route }) {
+  const [nickname, setNickname] = useState(route.params.nickname);
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [pwdErrMessage, setPwdErrMessage] = useState("");
+  const [confirmPwdErrMessage, setConfirmPwdErrMessage] = useState("");
+
+  function resetHandle() {
+    setNickname("");
+    setPassword("");
+    setConfirmPassword("");
+    setPwdErrMessage("");
+    setConfirmPwdErrMessage("");
+  }
+
+  const editSaveHandler = async () => {
+    console.log("000");
+    let flag = true;
+    if (password.length <= 8) {
+      console.log("111");
+      setPwdErrMessage("At least greater than 8 characters");
+      flag = false;
+    } else {
+      console.log("222");
+      setPwdErrMessage("");
+    }
+    if (password !== confirmPassword) {
+      console.log("333");
+      setConfirmPwdErrMessage("The password doesn't match");
+      flag = false;
+    } else {
+      console.log("444");
+      setConfirmPwdErrMessage("");
+    }
+    if (!flag) {
+      console.log("555");
+      return;
+    }
+
+    try {
+      //update the password
+      console.log("666");
+      await updatePassword(auth.currentUser, password);
+      updateUserInfo(nickname);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
-    <View>
-      <Text>EditSettings</Text>
-    </View>
-  )
+    <>
+      <View style={styles.infoContainer}>
+        <View style={styles.infoRow}>
+          <View>
+            <Label content="Nickname" customizedStyle={styles.label} />
+          </View>
+          <View style={{ flexDirection: "row" }}>
+            <TextInput
+              placeholder="Nick name"
+              value={nickname}
+              style={styles.label}
+              onChangeText={(changedText) => {
+                setNickname(changedText);
+              }}
+            />
+          </View>
+        </View>
+
+        <View style={styles.infoRow}>
+          <View>
+            <Label content="Password" customizedStyle={styles.label} />
+          </View>
+          <View style={{ flexDirection: "row" }}>
+            <TextInput
+              value={password}
+              placeholder="Password Reset"
+              style={styles.label}
+              secureTextEntry={true}
+              onChangeText={(numberInput) => {
+                setPassword(numberInput);
+              }}
+            />
+          </View>
+        </View>
+        {pwdErrMessage !== "" && <ErrorText message={pwdErrMessage} />}
+
+        <View style={styles.infoRow}>
+          <View>
+            <Label content="Confirm Password" customizedStyle={styles.label} />
+          </View>
+          <View style={{ flexDirection: "row" }}>
+            <TextInput
+              value={confirmPassword}
+              placeholder="Confirm Password"
+              style={styles.label}
+              secureTextEntry={true}
+              onChangeText={(numberInput) => {
+                setConfirmPassword(numberInput);
+              }}
+            />
+          </View>
+        </View>
+        {confirmPwdErrMessage !== "" && (
+          <ErrorText message={confirmPwdErrMessage} />
+        )}
+      </View>
+      <View style={{ flex: 2, justifyContent: "center" }}>
+        <PressableArea
+          areaPressed={resetHandle}
+          customizedStyle={styles.saveButton}
+        >
+          <Text style={styles.text}>Reset</Text>
+        </PressableArea>
+        <PressableArea
+          areaPressed={editSaveHandler}
+          customizedStyle={styles.saveButton}
+        >
+          <Text style={styles.text}>Save</Text>
+        </PressableArea>
+      </View>
+    </>
+  );
 }
+
+const styles = StyleSheet.create({
+  infoContainer: {
+    flex: 1,
+    padding: 10,
+    justifyContent: "space-around",
+  },
+  infoRow: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    borderBottomWidth: 1,
+    borderBottomColor: "#e5e5e5",
+  },
+  label: {
+    padding: 15,
+    fontSize: 16,
+    fontWeight: "normal",
+  },
+  saveButton: {
+    borderWidth: 1,
+    borderRadius: 5,
+    borderColor: "black",
+    alignItems: "center",
+    padding: 5,
+    marginLeft: "15%",
+    marginBottom: "12%",
+    width: "70%",
+  },
+});
