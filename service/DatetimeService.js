49588b311396c3edcc271af772406ef4ddfa7a14
yusun97 <yusun.apply@outlook.com>
2023-04-09 21:25:28 -0700

    format the month day minute second from 0 to 9

--- service/DatetimeService.js
+++ service/DatetimeService.js
@@ -9,16 +9,8 @@ export const convertNowDateToStr = () => {
 //covert passed date to date string
 export const convertDateToStr = (date) => {
   const year = date.getFullYear();
-  let month = parseInt(date.getMonth()) + 1;
-  if (month >= 1 && month <= 9) {
-    month = `0${month}`;
-  }
-  // console.log("month", month);
-  let day = date.getDate();
-  if (day >= 1 && day <= 9) {
-    day = `0${day}`;
-  }
-  // console.log(`${year}-${month}-${day}`);
+  const month = format(parseInt(date.getMonth()) + 1);
+  const day = format(date.getDate());
   return `${year}-${month}-${day}`;
 };
 
@@ -30,7 +22,14 @@ export const convertNowTimeToStrWithoutSeconds = () => {
 
 //convert passed date to time string hh:mm
 export const convertTimeToStrWithoutSeconds = (date) => {
-  const hour = date.getHours();
-  const minute = date.getMinutes();
+  const hour = format(date.getHours());
+  const minute = format(date.getMinutes());
   return `${hour}:${minute}`;
 };
+
+//if month/date/minute/second is within [0,9]
+function format(number) {
+  if (number >= 0 && number <= 9) {
+    return `0${number}`;
+  } else return `${number}`;
+}
